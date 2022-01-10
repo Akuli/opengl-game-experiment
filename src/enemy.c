b158edcd586e41796e97e62bccffa4eb314fbb4d
Akuli <akuviljanen17@gmail.com>
2022-01-10 13:07:59 +0200

    generate vertex data only once

--- src/enemy.c
+++ src/enemy.c
@@ -29,6 +29,51 @@ static vec4 point_on_surface(float t, float u)
 	return (vec4){ arcpoint3d.x, arcpoint3d.y, arcpoint3d.z, colorval };
 }
 
+static vec4 *get_vertex_data(int *npoints)
+{
+#define TSTEPS 110
+#define USTEPS 10
+	// *4 because a rectangle is represented as 2 triangles, and clipping can split each one in half
+	static vec4 vertexdata[TSTEPS*USTEPS*4][3];
+	static int ntriangles = 0;
+
+	if (ntriangles == 0) {
+		float pi = acosf(-1);
+
+		struct Plane xzplane = { .normal = {0,1,0}, .constant = 0 };
+
+		for (int tstep = 0; tstep < TSTEPS; tstep++) {
+			for (int ustep = 0; ustep < USTEPS; ustep++) {
+				// Min value of t tweaked so that the tip of enemy looks good
+				float t1 = lerp(-0.1f, 1, tstep/(float)TSTEPS);
+				float t2 = lerp(-0.1f, 1, (tstep+1)/(float)TSTEPS);
+				float u1 = lerp(0, pi, ustep/(float)USTEPS);
+				float u2 = lerp(0, pi, (ustep+1)/(float)USTEPS);
+#undef TSTEPS
+#undef USTEPS
+
+				vec4 a = point_on_surface(t1, u1);
+				vec4 b = point_on_surface(t1, u2);
+				vec4 c = point_on_surface(t2, u1);
+				vec4 d = point_on_surface(t2, u2);
+
+				ntriangles += plane_clip_triangle(xzplane, (vec4[]){a,b,c}, &vertexdata[ntriangles]);
+				ntriangles += plane_clip_triangle(xzplane, (vec4[]){d,b,c}, &vertexdata[ntriangles]);
+			}
+		}
+
+		// Scale it up
+		for (int i = 0; i < ntriangles; i++) {
+			vec4_mul_float_first3_inplace(&vertexdata[i][0], 2);
+			vec4_mul_float_first3_inplace(&vertexdata[i][1], 2);
+			vec4_mul_float_first3_inplace(&vertexdata[i][2], 2);
+		}
+	}
+
+	*npoints = 3*ntriangles;
+	return &vertexdata[0][0];
+}
+
 void enemy_render(struct Enemy *enemy, const struct Camera *cam)
 {
 	glUseProgram(enemy->shaderprogram);
@@ -39,51 +84,24 @@ void enemy_render(struct Enemy *enemy, const struct Camera *cam)
 		glGetUniformLocation(enemy->shaderprogram, "world2cam"),
 		1, true, &cam->world2cam.rows[0][0]);
 
-	static vec4 vertexdata[10000][3];
-	int ntriangles = 0;
-	float pi = acosf(-1);
-
-	int tsteps = 100, usteps = 10;
-	struct Plane xzplane = { .normal = {0,1,0}, .constant = 0 };
-
-	for (int tstep = 0; tstep < tsteps; tstep++) {
-		for (int ustep = 0; ustep < usteps; ustep++) {
-			// Min value of t tweaked so that the tip of enemy looks good
-			float t1 = lerp(-0.1f, 1, tstep/(float)tsteps);
-			float t2 = lerp(-0.1f, 1, (tstep+1)/(float)tsteps);
-			float u1 = lerp(0, pi, ustep/(float)usteps);
-			float u2 = lerp(0, pi, (ustep+1)/(float)usteps);
-
-			vec4 a = point_on_surface(t1, u1);
-			vec4 b = point_on_surface(t1, u2);
-			vec4 c = point_on_surface(t2, u1);
-			vec4 d = point_on_surface(t2, u2);
-
-			ntriangles += plane_clip_triangle(xzplane, (vec4[]){a,b,c}, &vertexdata[ntriangles]);
-			ntriangles += plane_clip_triangle(xzplane, (vec4[]){d,b,c}, &vertexdata[ntriangles]);
-		}
-	}
-
-	// Scale it up
-	for (int i = 0; i < ntriangles; i++) {
-		vec4_mul_float_first3_inplace(&vertexdata[i][0], 2);
-		vec4_mul_float_first3_inplace(&vertexdata[i][1], 2);
-		vec4_mul_float_first3_inplace(&vertexdata[i][2], 2);
-	}
-
 	if (enemy->vbo == 0) {
+		int npoints;
+		vec4 *vertexdata = get_vertex_data(&npoints);
+
 		glGenBuffers(1, &enemy->vbo);
 		SDL_assert(enemy->vbo != 0);
 		glBindBuffer(GL_ARRAY_BUFFER, enemy->vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexdata), NULL, GL_DYNAMIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vec4)*npoints, vertexdata, GL_DYNAMIC_DRAW);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 
+	int npoints;
+	get_vertex_data(&npoints);
+
 	glBindBuffer(GL_ARRAY_BUFFER, enemy->vbo);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertexdata), vertexdata);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_TRIANGLES, 0, 4*ntriangles);
+	glDrawArrays(GL_TRIANGLES, 0, npoints);
 	glDisableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glUseProgram(0);
