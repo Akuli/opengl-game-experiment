f46c40ba320daf7387316d4072929f9df8207146
Akuli <akuviljanen17@gmail.com>
2022-01-11 13:56:22 +0200

    Update src/map.cpp

--- src/map.cpp
+++ src/map.cpp
@@ -51,6 +51,7 @@ struct Section {
 
 static float uniform_random_float(float min, float max)
 {
+	// I tried writing this in "modern C++" style but that was more complicated
 	return lerp(min, max, std::rand() / (float)RAND_MAX);
 }
 
